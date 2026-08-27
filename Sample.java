407d8d7760c065011eee9f3d79b9120742bffc78
Md Umar <128371321+MDUMAR8202@users.noreply.github.com>
2026-08-27 12:38:04 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,5 +3,11 @@ public static void main(String[] args){
 
 System.out.println("Hello from Jenkins");
 System.out.println("Wellcome to Devops");
+  String s="java";
+  String str=new String("java");
+  System.out.println(s==str);
+   System.out.println(".......................");
+   System.out.println("wellcome code in jenkins");
+  
 }
 }
